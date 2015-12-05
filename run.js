85067741166c7a8a730ac7010770dda2fccdbe27
Victor Guo <guaiguaiyu@hotmail.com>
2015-12-05 23:22:47 +0800

    Update DB schema

--- run.js
+++ run.js
@@ -1,4 +1,35 @@
 var phantom = require('phantom');
+var uuid = require('uuid');
+var Schema = require('jugglingdb').Schema;
+
+var Record;
+
+function searchDatabase(url, cb) {
+    Record.findOne({where: {Url: url}, order: 'CreatedAt DESC'}, cb)
+}
+
+function saveToDatabase() {
+    Record.create()
+}
+
+function initDatabase() {
+    var schema = new Schema('mysql', {
+        host: '127.0.0.1',
+        username: 'root',
+        database: 'crawler'
+    });
+    Record = schema.define('Record', {
+        RecordId : {type: String, length: 36},
+        Url : {type: String, length: 255},
+        Content: {type: String, dataType: 'text'},
+        CreatedAt: {type: Date, dataType: 'timestamp'}
+    });
+
+    var newRecord = new Record();
+    newRecord.RecordId = uuid.v4();
+
+    newRecord.save();
+}
 
 phantom.create('--load-images=no', function(ph) {
     ph.createPage(function(page) {
